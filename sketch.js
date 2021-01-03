b2d53fafe17f4099f9e8742e42b7b8fc4f5b2ef7
AaravBhandari <71821426+AaravBhandari@users.noreply.github.com>
2021-01-03 10:44:15 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,38 @@
+var canvas, backgroundImage;
+
+var gameState = 0;
+var playerCount;
+var allPlayers;
+var distance = 0;
+var database;
+
+var form, player, game;
+
+var cars, car1, car2, car3, car4;
+function preload(){
+  police = loadAnimation("1.png","2.png")
+  thief = loadAnimation("1.png","2.png")
+  track= loadImage ("Track.png")
+}
+
+function setup(){
+  canvas = createCanvas(displayWidth - 20, displayHeight-30);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+
+function draw(){
+  if(playerCount === 2){
+    game.update(1);
+  }
+  if(gameState === 1){
+    clear();
+    game.play();
+  }
+  if (gameState === 2) {
+    game.end ()
+  }
+}
